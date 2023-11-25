e3ea997c73171b36311e8166c7c0c4388772bc32
Chu Hao Wen <45083436+chuhaow@users.noreply.github.com>
2023-11-25 16:22:56 +0900

    Added Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { Stack } from "@mui/material";
 
-function Sidebar() {
-  return (
-    <div>Sidebar</div>
-  )
-}
+import { categories } from "../utils/constants";
+
+const highlightedCate = "Home" //TODO: Make this update
+
+const Sidebar = () =>(
+  <Stack
+    direction={'row'}
+    sx={{
+      overflowY: "auto",
+      height: {sx: "auto", md: '95%'},
+      flexDirection: {md: 'column'},
+    }}
+  >
+    {categories.map((category) => (
+      <button 
+        className="category-btn"
+        style={{
+          background: category.name === highlightedCate && '#FC1503',
+          color: 'white'
+        }}
+        key={category.name}
+      >
+        <span style={{color: category.name === highlightedCate ? 'white' : 'red',
+          marginRight: '15px'}}>
+          {category.icon}
+        </span>
+        <span style={{opacity: category.name === highlightedCate ? '1' : '0'}}>
+          {category.name}
+        </span>
+      </button>
+    ))}
+  
+  </Stack>
+)
 
 export default Sidebar
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,29 @@
+import HomeIcon from '@mui/icons-material/Home';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import SchoolIcon from '@mui/icons-material/School';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+
+
+
 export const logo = 'https://github.com/chuhaow/ProjectGifs/blob/main/Grass/Grass.png';
+
+
+
+export const categories = [
+    { name: 'Home', icon: <HomeIcon />, },
+    { name: 'Music', icon: <MusicNoteIcon /> },
+    { name: 'Education', icon: <SchoolIcon />, },
+    { name: 'Podcast', icon: <GraphicEqIcon />, },
+    { name: 'Movie', icon: <OndemandVideoIcon />, },
+    { name: 'Gaming', icon: <SportsEsportsIcon />, },
+    { name: 'Live', icon: <LiveTvIcon />, },
+    { name: 'Sport', icon: <FitnessCenterIcon />, },
+    { name: 'Fashion', icon: <CheckroomIcon />, },
+    { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  ];
